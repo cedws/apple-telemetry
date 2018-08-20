4b8067227bf995b74d135220fbf8772c7d44ad81
Connor E <38229097+c-edw@users.noreply.github.com>
2018-08-21 00:12:17 +0100

    Add domains, fix script.

--- generate.py
+++ generate.py
@@ -5,16 +5,17 @@ from aiodns import DNSResolver
 loop = asyncio.get_event_loop()
 resolver = DNSResolver(loop=loop)
 
-def get_entries():
-    with open("blacklist", "r") as blacklist:
-        return blacklist.readlines()
+def get_entries(file):
+    with open(file, "r") as entries:
+        return entries.readlines()
 
-def remove_duplicates_from_blacklist(entries):
+def remove_duplicates(file):
     """
-    Remove duplicate entries from the blacklist.
+    Remove duplicate entries from a file.
     """
-    
-    with open("blacklist", "w") as new:
+
+    entries = get_entries(file)
+    with open(file, "w") as new:
         for entry in set(entries):
             new.write(entry)
 
@@ -32,26 +33,31 @@ async def generate_ip_blacklist(entries):
         for entry in entries:
             try:
                 ips = await resolver.query(entry.strip(), "A")
+                ip_blacklist.writelines(["{}\n".format(ip.host) for ip in ips])
             except Exception:
                 print("Failed to resolve domain {}.".format(entry.strip()))
-
-            ip_blacklist.writelines(["{}\n".format(ip.host) for ip in ips])
 
 def generate_cloaking_rules(entries):
     with open("cloaking-rules", "w") as cloaking:
         cloaking.writelines(["{} 0.0.0.0\n".format(entry.strip()) for entry in entries])
 
 async def main():
-    print("Removing duplicates from blacklist.")
-    remove_duplicates_from_blacklist(get_entries())
-
     print("Sorting blacklist.")
-    sort_blacklist(get_entries())
+    sort_blacklist(get_entries("blacklist"))
 
     print("Generating IP blacklist.")
-    await generate_ip_blacklist(get_entries())
+    await generate_ip_blacklist(get_entries("blacklist"))
 
     print("Generating cloaking rules.")
-    generate_cloaking_rules(get_entries())
+    generate_cloaking_rules(get_entries("blacklist"))
+
+    print("Removing duplicates from blacklist.")
+    remove_duplicates("blacklist")
+
+    print("Removing duplicates from IP blacklist.")
+    remove_duplicates("ip-blacklist")
+
+    print("Removing duplicates from cloaking rules.")
+    remove_duplicates("cloaking-rules")
 
 loop.run_until_complete(main())
